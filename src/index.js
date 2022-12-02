6e76a0c9af708405d6baedb282ef548b46400379
Tristan Davis <Tristandavis888@gmail.com>
2022-12-01 20:44:29 -0800

    added Caddyfile, caddy.service file, hello_web.service file, edited html index.html

--- src/index.js
+++ src/index.js
@@ -1,18 +1,15 @@
 // Require the framework and instantiate it
 const fastify = require("fastify")({ logger: true });
-const fs = require("fs").promises;
 
 // Declare a route
-fastify.get("/", async (request, reply) => {
-  const indx = await fs.readFile("../html/index.html", { encoding: "utf-8" });
-  reply.type("text/html");
-  return indx;
+fastify.get("/api", async (request, reply) => {
+        return { hello: 'Server 1' }
 });
 
 // Run the server!
 const start = async () => {
   try {
-    await fastify.listen({ port: 3000 });
+    await fastify.listen({ port: 5050 });
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
